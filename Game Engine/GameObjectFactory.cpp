caa9297d8b2e111fa24b631736e06f94a79ad2bb
JayTheDervish <yaseen05@gmail.com>
2017-11-05 15:46:29 -0800

    Added GameObjectFactory "body" and started implementation.

--- Game Engine/GameObjectFactory.cpp
+++ Game Engine/GameObjectFactory.cpp
@@ -4,31 +4,31 @@ Copyright (C) 2017 DigiPen Institute of Technology.
 Reproduction or disclosure of this file or its contents without the prior
 written consent of DigiPen Institute of Technology is prohibited.
 
-File Name:	GameObjectFactory.h
-Purpose:	Contains the declaration for the GameObjectFactory class.
+File Name:	GameObjectFactory.cpp
+Purpose:	Contains the definition for the GameObjectFactory class.
 Language:	C++
 Platform:	Windows, MS C++ compiler, VS v140
-Project:	jcoleman_CS529_GameObjectFactoryh
+Project:	jcoleman_CS529_GameObjectFactorycpp
 Author:		Jay Coleman j.coleman 60000217
 Creation date: 10/26/2017
 
 - End Header --------------------------------------------------------*/
 
-#pragma once
+#include "GameObjectFactory.h"
 
-#include "Components\GameObject.h"
-
-#ifndef GAMEOBJECTFACTORY_H
-#define GAMEOBJECTFACTORY_H
-
-class GameObjectFactory
+GameObjectFactory::GameObjectFactory()
 {
-public:
-	GameObjectFactory();
-	~GameObjectFactory();
+}
 
-	GameObject * CreateObject(std::string type);
-private:
-};
+GameObjectFactory::~GameObjectFactory()
+{
+}
 
-#endif // !GAMEOBJECTFACTORY_H
+GameObject * GameObjectFactory::CreateObject(std::string type)
+{
+	GameObject * newObj = NULL;
+
+
+
+	return newObj;
+}
