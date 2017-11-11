8eea1e159b4d0f0f291f5dcd057455631b33d4f6
JayTheDervish <yaseen05@gmail.com>
2017-11-11 13:44:42 -0800

    Finally fixed my problem from last night! Even added a wall object to prove it.

--- Game Engine/GameObjectManager.cpp
+++ Game Engine/GameObjectManager.cpp
@@ -33,7 +33,7 @@ void GameObjectManager::Load(nlohmann::json filename)
 {
 	nlohmann::json o; //Objects
 	for (nlohmann::json::iterator it = filename.begin(); it != filename.end(); ++it) {
-		for (int i = 0;/*Put a decent condition here*/ ; ++i)
+		for (int i = 0; i < filename[it.key()].size() ; ++i)
 		{
 			o = filename[it.key()][i];
 			objects.push_back(GOFactory->CreateObject(o));
